f4a0d131cc967835eedc3455f323c678f17dc0f3
Simon Lammer <lammer.simon@gmail.com>
2018-05-01 14:14:44 +0200

    create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+window.Persistence = new function() {
+  var _persistenceKey = 'github.com/SimonLammer/anki-persistence';
+  var _isAvailable = false;
+  try {
+    if (typeof(window.sessionStorage) === 'object') {
+      _isAvailable = true;
+      this.store = function(data) {
+        sessionStorage.setItem(_persistenceKey, JSON.stringify(data));
+      };
+      this.load = function() {
+        return JSON.parse(sessionStorage.getItem(_persistenceKey));
+      }
+    }
+  } catch(err) {}
+  if (!_isAvailable) {
+    try {
+      if (typeof(window.py) === 'object') {
+        _isAvailable = true;
+        this.store = function(data) {
+          py[_persistenceKey] = data;
+        };
+        this.load = function() {
+          return py[_persistenceKey];
+        };
+      }
+    } catch(err) {}
+  }
+  this.isAvailable = function() {
+    return _isAvailable;
+  };
+}();
