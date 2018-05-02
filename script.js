f625e8a33ca099359de4e914042ff22b25d6a2e9
Simon Lammer <lammer.simon@gmail.com>
2018-05-02 21:42:02 +0200

    Only instantiate Persistence once

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-window.Persistence = new function() {
-  var _persistenceKey = 'github.com/SimonLammer/anki-persistence';
-  var _isAvailable = false;
-  try {
-    if (typeof(window.sessionStorage) === 'object') {
-      _isAvailable = true;
-      this.store = function(data) {
-        sessionStorage.setItem(_persistenceKey, JSON.stringify(data));
-      };
-      this.load = function() {
-        return JSON.parse(sessionStorage.getItem(_persistenceKey));
-      }
-    }
-  } catch(err) {}
-  if (!_isAvailable) {
+if (typeof(window.Persistence) === 'undefined') {
+  window.Persistence = new function() {
+    var _persistenceKey = 'github.com/SimonLammer/anki-persistence';
+    var _isAvailable = false;
     try {
-      if (typeof(window.py) === 'object') {
+      if (typeof(window.sessionStorage) === 'object') {
         _isAvailable = true;
         this.store = function(data) {
-          py[_persistenceKey] = data;
+          sessionStorage.setItem(_persistenceKey, JSON.stringify(data));
         };
         this.load = function() {
-          return py[_persistenceKey] || null;
-        };
+          return JSON.parse(sessionStorage.getItem(_persistenceKey));
+        }
       }
     } catch(err) {}
-  }
-  this.isAvailable = function() {
-    return _isAvailable;
-  };
-}();
+    if (!_isAvailable) {
+      try {
+        if (typeof(window.py) === 'object') {
+          _isAvailable = true;
+          this.store = function(data) {
+            py[_persistenceKey] = data;
+          };
+          this.load = function() {
+            return py[_persistenceKey] || null;
+          };
+        }
+      } catch(err) {}
+    }
+    this.isAvailable = function() {
+      return _isAvailable;
+    };
+  }();
+}
